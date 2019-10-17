73e40bcddd4a264a53e2b92db9a32b8b82bcc7de
zia <zia.tokhi@gmail.com>
2019-10-17 04:07:56 +0200

    App added

---
+++ BoxingTire.App/BoxingTire.App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BoxingTire.App.Models;
+
+namespace BoxingTire.App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
