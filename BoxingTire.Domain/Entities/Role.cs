8a249a1b32a83c7e0bbde9d6af77ce933e2725a8
zia <zia.tokhi@gmail.com>
2019-10-17 03:25:55 +0200

    Add project files.

---
+++ BoxingTire.Domain/Entities/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxingTire.Domain.Entities
+{
+    class Role
+    {
+        int Id { get; set; }
+
+       string Name { get; set; }
+
+        bool IsEnable { get; set; }
+    }
+}
---
+++ BoxingTire.Domain/Entities/UserAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxingTire.Domain.Entities
+{
+  public  class UserAccount
+    {
+        Guid Id { get; set; }
+        string Name  { get; set; }
+        string EmailAddress { get; set; }
+        string Password { get; set; }
+        bool IsEnable { get; set; }
+      int Role { get; set; }
+        DateTime UpdatedOn{ get; set; }
+
+    }
+}
